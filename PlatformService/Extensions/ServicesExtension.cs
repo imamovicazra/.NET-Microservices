aeb5745a01c59e654aaa5f4f2d3e58f0752a4369
imamovicazra <azra.imamovic998@gmail.com>
2022-07-18 09:42:40 +0200

    Added DB and Swagger configuration

---
+++ PlatformService/Extensions/ServicesExtension.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
+namespace PlatformService.Extensions
+{
+    public static class ServicesExtension
+    {
+
+        public static IServiceCollection AddServices(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+
+            if (builder.Environment.IsProduction())
+            {
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn"));
+                });
+            }
+            else
+            {
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMem");
+                });
+            }
+
+            services.AddSwaggerConfiguration();           
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());           
+            return services;
+        }
+    }
+}
---
+++ PlatformService/Extensions/SwaggerConfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace PlatformService.Extensions
+{
+    public class SwaggerConfiguration
+    {
+    }
+}
