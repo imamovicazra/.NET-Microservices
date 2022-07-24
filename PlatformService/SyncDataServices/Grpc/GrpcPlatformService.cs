30de2ae7d1e2cbff62316263aba78b37307bd586
imamovicazra <azra.imamovic998@gmail.com>
2022-07-24 16:25:18 +0200

    Added Platforms proto file

---
+++ PlatformService/SyncDataServices/Grpc/GrpcPlatformService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using PlatformService.Repositories;
+
+namespace PlatformService.SyncDataServices.Grpc
+{
+    public class GrpcPlatformService : GrpcPlatform.GrpcPlatformBase
+    {
+        private readonly IPlatformRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GrpcPlatformService(IPlatformRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public override Task<PlatformResponse> GetAllPlatforms(GetAllRequest request, ServerCallContext context)
+        {
+            var response = new PlatformResponse();
+            var platforms = _repository.GetAllPlatforms();
+            foreach (var item in platforms)
+            {
+                response.Platform.Add(_mapper.Map<GrpcPlatformModel>(item));
+            }
+            return Task.FromResult(response);
+        }
+    }
+}
